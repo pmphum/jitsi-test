56973ce142886ab57f63123601e48bbcd6264208
Athiphum Chaisri (Phum) <pmphum@hotmail.com>
2022-07-21 09:51:42 +0700

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,22 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { JitsiComponent } from './pages/jitsi/jitsi.component';
+// import { JitsiMeetJS } from '@solyd/lib-jitsi-meet';
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    JitsiComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+
+
   ],
   providers: [],
   bootstrap: [AppComponent]
